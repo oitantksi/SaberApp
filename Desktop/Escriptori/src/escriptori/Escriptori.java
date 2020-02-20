278d2a09e153540a4c2f29f479bdbfadd4acb010
Montse Camps <m_camps7@hotmail.com>
2020-02-20 19:58:00 +0100

    1a prova

---
+++ Desktop/Escriptori/src/escriptori/Escriptori.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package escriptori;
+
+/**
+ *
+ * @author montse
+ */
+public class Escriptori {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
