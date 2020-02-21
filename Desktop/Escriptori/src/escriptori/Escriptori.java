60fc90077bfbb025073565890dad4cb34aac4146
Montse Camps <m_camps7@hotmail.com>
2020-02-21 16:53:30 +0100

    1a prova

--- Desktop/Escriptori/src/escriptori/Escriptori.java
+++ Desktop/Escriptori/src/escriptori/Escriptori.java
@@ -15,7 +15,7 @@ public class Escriptori {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola món!");
     }
     
 }
