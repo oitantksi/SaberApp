1155be739d2f79aff533052bf47e3f974aeec936
regression85 <ramonhernandezrhf@hotmail.com>
2020-04-11 13:08:35 +0200

    Server: cambio de lombok por getters y setters

--- Server/src/main/java/com/omega/server/saberapp/entity/Respuesta.java
+++ Server/src/main/java/com/omega/server/saberapp/entity/Respuesta.java
@@ -10,15 +10,16 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
 @Entity
 @Table(name="respuestas")
 public class Respuesta implements Serializable {
+
+	
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6678057026491452955L;
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
@@ -36,7 +37,65 @@ public class Respuesta implements Serializable {
 	@Column(name="alumno_id")
 	Long userId;
 	
+//	@JsonFormat(pattern="yyyy-MM-dd")
 	@Column(name="fecha_respuesta")
 	Timestamp fechaRespuesta;
+	
+	public Respuesta(Long id, Long respuesta, Long preguntaId, Long userId, Timestamp fechaRespuesta) {
+		super();
+		this.id = id;
+		this.respuesta = respuesta;
+		this.preguntaId = preguntaId;
+		this.userId = userId;
+		this.fechaRespuesta = fechaRespuesta;
+	}
+
+	public Respuesta() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getRespuesta() {
+		return respuesta;
+	}
+
+	public void setRespuesta(Long respuesta) {
+		this.respuesta = respuesta;
+	}
+
+	public Long getPreguntaId() {
+		return preguntaId;
+	}
+
+	public void setPreguntaId(Long preguntaId) {
+		this.preguntaId = preguntaId;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public Timestamp getFechaRespuesta() {
+		return fechaRespuesta;
+	}
+
+	public void setFechaRespuesta(Timestamp fechaRespuesta) {
+		this.fechaRespuesta = fechaRespuesta;
+	}
+	
+	
+	
 	
 }
--- Server/src/test/java/com/omega/server/saberapp/persistence/repository/RespuestaRepositoryIT.java
+++ Server/src/test/java/com/omega/server/saberapp/persistence/repository/RespuestaRepositoryIT.java
@@ -35,6 +35,12 @@ public class RespuestaRepositoryIT {
 		
 		
 	}
+	@Test
+	public void grabarRespuestaAlumno() {
+		
+		
+		
+	}
 	
 
 }
