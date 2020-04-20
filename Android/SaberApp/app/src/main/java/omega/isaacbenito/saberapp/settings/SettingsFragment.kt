f714bcbe19f103a302fac26ef5f85469c1baf0a3
oitantksi <oitantksi@gmail.com>
2020-04-20 09:46:54 +0200

    added about activity

---
+++ Android/SaberApp/app/src/main/java/omega/isaacbenito/saberapp/settings/SettingsFragment.kt
@@ -0,0 +1,18 @@
+package omega.isaacbenito.saberapp.settings
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+import dagger.android.support.DaggerFragment
+
+/**
+ * TODO
+ *
+ * @author Isaac Benito
+ **/
+class SettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        TODO("Not yet implemented")
+    }
+
+
+}
---
+++ Android/SaberApp/app/src/main/java/omega/isaacbenito/saberapp/ui/AboutFragment.kt
@@ -0,0 +1,90 @@
+package omega.isaacbenito.saberapp.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import com.google.android.material.tabs.TabLayoutMediator.TabConfigurationStrategy
+import omega.isaacbenito.saberapp.R
+import omega.isaacbenito.saberapp.databinding.FragmentAboutBinding
+
+
+private val about_pages = listOf(
+    R.layout.fragment_about_credits,
+    R.layout.fragment_about_contribution,
+    R.layout.fragment_about_license
+)
+
+private val about_titles = listOf(
+    R.string.credits,
+    R.string.contributing,
+    R.string.licensing
+)
+
+/**
+ * TODO
+ *
+ * @author Isaac Benito
+ **/
+class AboutFragment : Fragment() {
+
+    private lateinit var binding: FragmentAboutBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_about, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.pager.adapter = AboutPagerAdapter(this)
+
+        TabLayoutMediator(binding.tabLayout, binding.pager,
+            TabConfigurationStrategy { tab: TabLayout.Tab, position: Int ->
+                tab.setText(about_titles.get(position)
+                )
+            }
+        ).attach()
+    }
+
+}
+
+class AboutPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
+
+    override fun getItemCount(): Int = about_pages.size
+
+    override fun createFragment(position: Int): Fragment {
+        val fragment = PageFragment()
+        val args = Bundle()
+        args.putInt(PageFragment.PAGE, position)
+        fragment.arguments = args
+        return fragment
+    }
+}
+
+
+
+class PageFragment: Fragment() {
+    companion object {
+        const val PAGE = "PAGE"
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val page = arguments?.getInt(PAGE) ?: throw NullPointerException()
+
+        return inflater.inflate(about_pages[page], container)
+    }
+}
